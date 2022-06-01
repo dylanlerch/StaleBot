b965e93c6b07501706ecd2aa079d06a9522d2373
Dylan Lerch <dylanlerch@hey.com>
2022-06-01 12:07:23 +1000

    Some work in progress, this commit will disappear

--- src/StaleBot/Program.cs
+++ src/StaleBot/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Octokit;
+
+namespace StaleBot
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var pat = await File.ReadAllTextAsync("pat.txt");
+            var owner = await File.ReadAllTextAsync("user.txt");
+            var repo = await File.ReadAllTextAsync("repo.txt");
+
+            var client = new GitHubClient(new ProductHeaderValue("StaleBot"))
+            {
+                Credentials = new Credentials(pat)
+            };
+
+            var repository = await client.Repository.Get(owner, repo);
+            var openPullRequests = await client
+                .Repository
+                .PullRequest
+                .GetAllForRepository(owner, repo, new PullRequestRequest {State = ItemStateFilter.Open});
+
+            foreach (var pullRequest in openPullRequests)
+            {
+                var currentBase = await client.Repository.Commit.Get(owner, repo, $"refs/heads/{pullRequest.Base.Ref}");
+                var difference = await client.Repository.Commit.Compare(owner, repo, pullRequest.Head.Sha, currentBase.Sha);
+
+                var pullRequestCommitsInDifference = new List<GitHubCommit>();
+
+                foreach (var commit in difference.Commits)
+                {
+                    if (await IsPrCommit(repository, client, commit))
+                    {
+                        pullRequestCommitsInDifference.Add(commit);
+                    }
+                }
+            }
+        }
+
+        static async Task<bool> IsPrCommit(Repository repo, GitHubClient client, GitHubCommit commit)
+        {
+            if (commit.Parents.Count == 2)
+            {
+                var results = await client.Search.SearchIssues(new SearchIssuesRequest
+                {
+                    Type = IssueTypeQualifier.PullRequest,
+                    
+                    Parameters =
+                    {
+                        // { "repository_id", repo.Id.ToString() },
+                        // { "q", commit.Sha }
+                    },
+                });
+
+                return results.TotalCount > 0;
+            }
+
+            return false;
+        }
+    }
+}
